020c6caa2294ddd9518f3df9c96548f52270d6a1
Judah Hunger <judah.hunger@gmail.com>
2018-01-10 13:20:44 -0800

    added console.logs and updating first prompts

--- js/main.js
+++ js/main.js
@@ -7,24 +7,28 @@ var myOrange = prompt('Do you think i like the color orange? Please reply with Y
     } else{
         alert('Meh I could go either');
     }
-
-var myAge = parseInt(prompt('How old do you think i am?'));
-    if(myAge === 32){
-        alert('That\'s my age!!');
-    } else if(myAge < 32){
-        alert('I am older')
-    } else{
-        alert('nope i\'m not that old yet')
-    }
-
-var countriesVisited = prompt('How many countries do you think I\'v been too?');
-    if(countriesVisited === 17){
-        alert('Yep mostly the middle east and Asia.');
-    } else
+ console.log('The user entered ' + myOrange + 'for quetion 1');
 
 var mySiblings = prompt('Do you think i have siblings');
+    if (mySiblings ==='YES'){
+        alert('YES I have a Brother and a Sister.')
+    } else{
+        alert('Sometimes I wish i was an only child!!')
+    }
+Console.log('the user entered ' + mySiblings + '. For question 2.')
 
 
-var theColorPurple = prompt('Do you think i like, the color purple?')
+// var theColorPurple = prompt('Do you think i like, the color purple?')
 
-
+// var myAge = parseInt(prompt('How old do you think i am?'));
+//     if(myAge === 32){
+//         alert('That\'s my age!!');
+//     } else if(myAge < 32){
+//         alert('I am older')
+//     } else{
+//         alert('nope i\'m not that old yet')
+//     }
+// var countriesVisited = prompt('How many countries do you think I\'v been too?');
+//     if(countriesVisited === 17){
+//         alert('Yep mostly the middle east and Asia.');
+//     } else
