ecf9acfddbd67f1df11dd91a3d7d77ae198e03bf
Judah Hunger <judah.hunger@gmail.com>
2018-01-10 08:20:52 -0800

    added js first part and quetions to make in html

---
+++ js/main.js
@@ -0,0 +1,30 @@
+'use strict';
+alert('help is there anybody out there? Just nod if you can hear me.');
+
+var myOrange = prompt('Do you think i like the color orange? Please reply with YES or NO');
+    if (myOrange === 'YES'){
+        alert('I don\'t dislike orange.');
+    } else{
+        alert('Meh I could go either');
+    }
+
+var myAge = parseInt(prompt('How old do you think i am?'));
+    if(myAge === 32){
+        alert('That\'s my age!!');
+    } else if(myAge < 32){
+        alert('I am older')
+    } else{
+        alert('nope i\'m not that old yet')
+    }
+
+var countriesVisited = prompt('How many countries do you think I\'v been too?');
+    if(countriesVisited === 17){
+        alert('Yep mostly the middle east and Asia.');
+    } else
+
+var mySiblings = prompt('Do you think i have siblings');
+
+
+var theColorPurple = prompt('Do you think i like, the color purple?')
+
+
