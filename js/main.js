4061b9b61b16de605bff8b8507e9d56ac4a3ff45
Judah Hunger <judah.hunger@gmail.com>
2018-01-10 14:32:03 -0800

    update to questions for lab 2 html and js

--- js/main.js
+++ js/main.js
@@ -1,22 +1,37 @@
 'use strict';
 alert('help is there anybody out there? Just nod if you can hear me.');
 
-var myOrange = prompt('Do you think i like the color orange? Please reply with YES or NO');
-    if (myOrange === 'YES'){
+var myOrange = prompt('Do you think i like the color orange? Please reply with YES or NO').toUpperCase();
+    if (myOrange === 'YES' || myOrange === 'Y'){
         alert('I don\'t dislike orange.');
     } else{
         alert('Meh I could go either');
     }
- console.log('The user entered ' + myOrange + 'for quetion 1');
+ console.log('The user entered ' + myOrange + ' for quetion 1');
 
-var mySiblings = prompt('Do you think i have siblings');
+var mySiblings = prompt('Do you think i have siblings? YES or No').toUpperCase();
     if (mySiblings ==='YES'){
-        alert('YES I have a Brother and a Sister.')
+        alert('YES I have a brother and a sister.');
     } else{
-        alert('Sometimes I wish i was an only child!!')
+        alert('Sometimes I wish i was an only child!!');
     }
-Console.log('the user entered ' + mySiblings + '. For question 2.')
+console.log('the user entered ' + mySiblings + '. For question 2.');
 
+var countriesVisited = prompt('Do you think I\'ve been to over 10 countries? yes or no').toLowerCase();
+    if (countriesVisited === 'yes'|| countriesVisited === "y"){
+        alert('Yep I\'ve been to 17 different countries thanks to uncle Sam');
+    } else{
+        alert('Actually I\'ve been to 17 countries');
+    }
+console.log('the user entered' + countriesVisited + '. For question 3');
+
+var favoriteFruit = prompt('Do you think I like pineapple? y or n').toLowerCase();
+    if (favoriteFruit === 'yes' || favoriteFruit === 'y'){
+        alert('I Love pineapple!!')
+    } else{
+        alert('Pineapple is my favorite fruit!')
+    }
+console.log('the user entered' + favoriteFruit + '. On question 4');
 
 // var theColorPurple = prompt('Do you think i like, the color purple?')
 
@@ -28,7 +43,3 @@ Console.log('the user entered ' + mySiblings + '. For question 2.')
 //     } else{
 //         alert('nope i\'m not that old yet')
 //     }
-// var countriesVisited = prompt('How many countries do you think I\'v been too?');
-//     if(countriesVisited === 17){
-//         alert('Yep mostly the middle east and Asia.');
-//     } else
